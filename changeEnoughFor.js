7605c67122f4345f575c97df13fa0aeeb521cd8d
stelio02 <spapa21@epoka.edu.al>
2024-04-09 20:57:04 +0200

    Update changeEnoughFor.js

--- changeEnoughFor.js
+++ changeEnoughFor.js
@@ -23,10 +23,51 @@ try {
   console.log(changeEnough([30, 40, 20, 5], 12.55)); // true
   console.log(changeEnough([10, 0, 0, 50], 3.85)); // false
   console.log(changeEnough([1, 0, 5, 219], 19.99)); // false
+
   console.log(changeEnough(1, 0, 5, 219, 19.99)); // error
   console.log(changeEnough([1.5, 0, 5, 219], 19.99)); // error
   console.log(changeEnough([1, 0, 5, 219], -19.99)); // error
   console.log(changeEnough([1, 0, 5, 219], 'S')); // error
+  console.log(changeEnough([-1, 0, 5, 219], 19.99)); // error
+
 } catch (error) {
   console.error(error.message);
 }
+
+/*
+try {
+  console.log(changeEnough([2, 100, 0, 0], 14.11)); // false
+  console.log(changeEnough([0, 0, 20, 5], 0.75)); // true
+  console.log(changeEnough([30, 40, 20, 5], 12.55)); // true
+  console.log(changeEnough([10, 0, 0, 50], 3.85)); // false
+  console.log(changeEnough([1, 0, 5, 219], 19.99)); // false
+
+  console.log(changeEnough(1, 0, 5, 219, 19.99)); // error
+} catch (error) {
+  console.error(error.message);
+}
+
+try {
+  console.log(changeEnough([1.5, 0, 5, 219], 19.99)); // error
+} catch (error) {
+  console.error(error.message);
+}
+
+try {
+  console.log(changeEnough([1, 0, 5, 219], -19.99)); // error
+} catch (error) {
+  console.error(error.message);
+}
+
+try {
+  console.log(changeEnough([1, 0, 5, 219], 'S')); // error
+} catch (error) {
+  console.error(error.message);
+}
+
+try {
+  console.log(changeEnough([-1, 0, 5, 219], 19.99)); // error
+} catch (error) {
+  console.error(error.message);
+}
+*/
