8063ca9542e815434883f6b02e7229615b676bd6
stelio02 <spapa21@epoka.edu.al>
2024-04-27 13:33:13 +0200

    Update test.js

---
+++ test.js
@@ -0,0 +1,29 @@
+function putWater(){
+    console.log("Vura ujin");
+    setTimeout(()=>
+console.log("U zie uji"),
+2000)
+}
+
+function watchTV(){
+    console.log("Pash TV");
+}
+
+function boilPasta(){
+    console.log("Vura makaronat");
+}
+
+function eatPasta(){
+    console.log("Hengra");
+}
+
+function main (){
+
+putWater();
+watchTV();
+boilPasta();
+eatPasta();
+
+}
+
+main();
