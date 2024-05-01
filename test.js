c2b49a6ed58240cf2a214c741a09db1893968f9e
stelio02 <spapa21@epoka.edu.al>
2024-05-01 12:46:54 +0200

    update test.js

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+/*
 function putWater(){
     console.log("Vura ujin");
     setTimeout(()=>
@@ -27,3 +28,52 @@ eatPasta();
 }
 
 main();
+*/
+
+// Simulated asynchronous functions to simulate boiling water, cooking pasta, making sauce, and eating
+function boilWater(callback) {
+    console.log("Putting water to boil for pasta...");
+    setTimeout(() => {
+        console.log("Watching TV while waiting for water to boil...");
+    } ,1000);
+    setTimeout(() => {
+        console.log("Water is boiling!");
+        callback();
+    }, 3000); // Simulating boiling water for 3 seconds
+}
+
+function cookPasta(callback) {
+    console.log("Pouring pasta into boiling water...");
+    setTimeout(() => {
+        console.log("Pasta is ready!");
+        callback();
+    }, 5000); // Simulating cooking pasta for 5 seconds
+}
+
+function makeSauce(callback) {
+    console.log("Making sauce...");
+    setTimeout(() => {
+        console.log("Sauce is ready!");
+        callback();
+    }, 4000); // Simulating making sauce for 4 seconds
+}
+
+function eat() {
+    boilWater(() => {
+        cookPasta(() => {
+            makeSauce();
+        });
+    });
+    console.log("Eating delicious pasta with sauce while watching TV.");
+}
+eat();
+
+/* Starting the process
+boilWater(() => {
+    cookPasta(() => {
+        makeSauce(() => {
+            eat();
+        });
+    });
+});
+*/
