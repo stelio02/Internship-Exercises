eebf340154cd32b5046742c994057a00d94edfff
stelio02 <spapa21@epoka.edu.al>
2024-05-03 11:38:30 +0200

    Update asyncPasta.js

---
+++ asyncPasta.js
@@ -0,0 +1,56 @@
+function watchTV() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Watching TV while waiting for water to boil...");
+            resolve();
+        }, 1000);
+    });
+}
+
+async function putWater() {
+    console.log("Putting water to boil for pasta...");
+    await watchTV();
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Water is boiling!");
+            resolve();
+        }, 2000);
+    });
+}
+
+async function makeSauce() {
+    console.log("Making sauce...");
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Sauce is ready!")
+            resolve();
+        }, 500);
+    });
+}
+
+async function cookPasta() {
+    console.log("Pouring pasta into boiling water...");
+    await makeSauce();
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Pasta is ready!")
+            resolve();
+        }, 4000);
+    });
+}
+
+function eatPasta() {
+    console.log("Eating delicious pasta with sauce while watching TV.");
+}
+
+async function main() {
+    try {
+        await putWater();
+        await cookPasta();
+        eatPasta();
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+}
+
+main();
