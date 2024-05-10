fdf8a68a9d20d502252f5b63a4d5814d0d4c8775
stelio02 <spapa21@epoka.edu.al>
2024-05-10 10:54:57 +0200

    Update userCallback.js

--- userCallback.js
+++ userCallback.js
@@ -18,46 +18,13 @@ function fetchUserPosts(userId, callback) {
     }, 1500); 
 };
 
-/*
-function fetchDataAndProcess() {
-    fetchUserData(userData => {
-        console.log('User Data:', userData);
-        fetchUserPosts(userData, userPosts => {
-            console.log('User Posts:', userPosts);
-            
-            console.log('Combined Data:', { userData, userPosts });
-        });
-    });
-};
-
-function fetchDataAndProcess() {
-    fetchUserData(userData => {
-        console.log('User Data:');
-        userData.forEach(user => {
-            console.log(`id: ${user.id}, name: ${user.name}, username: ${user.username}`);
-        });
-        fetchUserPosts(userData[0].id, userPosts => { // Assuming the first user's id is used for posts
-            console.log('\nUser Posts:');
-            userPosts.forEach(post => {
-                console.log(`id: ${post.id}, userId: ${post.userId}, title: ${post.title}`);
-            });
-
-            console.log('\nCombined Data:');
-            userData.forEach(user => {
-                const userPost = userPosts.find(post => post.userId === user.id);
-                console.log(`User ${user.name}'s Data: id: ${user.id}, name: ${user.name}, username: ${user.username}, title: ${userPost.title}`);
-            });
-        });
-    });
-};
-*/
 function fetchDataAndProcess() {
     fetchUserData(userData => {
         console.log('Combined Data:');
         userData.forEach(user => {
             fetchUserPosts(user.id, userPosts => {
                 const userPost = userPosts.find(post => post.userId === user.id);
-                console.log(`User Data: id: ${user.id}, name: ${user.name}, username: ${user.username}, Post: id: ${userPost.id}, userId: ${userPost.userId}, title: ${userPost.title}`);
+                console.log(`User Data: id: ${user.id}, name: ${user.name}, username: ${user.username}, \n Post: id: ${userPost.id}, userId: ${userPost.userId}, title: ${userPost.title}`);
             });
         });
     });
