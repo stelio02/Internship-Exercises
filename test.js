4f17bc187e6acafa76da8850c20da4aff4e3c78e
stelio02 <spapa21@epoka.edu.al>
2024-05-21 23:50:42 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,40 @@
+let content = "stelio";
 
+function readFile(callback) {
+  setTimeout(() => {
+    console.log(content);
+    callback();
+  }, 3000);
+}
+
+function transformContent(content, callback) {
+  setTimeout(() => {
+    console.log(content.toUpperCase());
+    callback();
+  }, 2000);
+}
+
+function saveFile(callback) {
+  setTimeout(() => {
+    callback("File saved successfully");
+  }, 3000);
+}
+
+function logCompletion(message) {
+  setTimeout(() => {
+    console.log(message);
+  }, 1000);
+}
+
+function processFile() {
+  console.log("Processing file...");
+  readFile(() => {
+    transformContent(() => {
+      saveFile(() => {
+        logCompletion(`Processed file with content: ${transformMessage}`);
+      });
+    });
+  });
+}
+
+processFile();
